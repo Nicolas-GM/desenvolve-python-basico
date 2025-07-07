eaa41a3cac38124232e3c89b55118d7e2410cb2f
Nicolas-GM <nicolas.marques@pditabira.com>
2025-07-07 11:01:54 -0300

    Add files via upload

---
+++ Módulo - 3/Exercícios - Expressões relacionais e lógicas/Exercícios - Condicionais/aula2_questao1.py
@@ -0,0 +1,7 @@
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+
+if (num1 + num2) % 2 == 0:
+    print("O resultado da soma desses dois números é par.")
+else:
+    print("O resultado da soma desses dois números é ímpar.")
